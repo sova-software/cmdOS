eaaf44414b47cdd9588c3a50c4d4cbf7283290f8
usarana <43117189+usarana@users.noreply.github.com>
2020-10-04 09:57:38 +0300

    Update e.bat

--- e.bat
+++ e.bat
@@ -1,5 +1,5 @@
 @echo off
-title cmdOS 3
+title cmdOS 4
 cd .acc
 if exist temp.txt (del temp.txt && goto l) else (goto l)
 :l
@@ -10,10 +10,11 @@ echo.
 :qn
 cd..
 echo Here are some programs you can use.
-cmdmenusel E870 "Timer" "Calculator" "Command Line"
+cmdmenusel E870 "Timer" "Calculator" "Command Line" "Text editor"
 if %errorlevel%==1 goto t
 if %errorlevel%==2 goto c
 if %errorlevel%==3 goto m
+if %errorlevel%==4 goto k
 :t
 start Library\timer.bat
 exit
@@ -22,4 +23,7 @@ start Library\calculator.bat
 exit
 :m
 start Library\command.bat %1
+exit
+:k
+start Library/Text editor
 exit
