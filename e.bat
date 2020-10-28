7dd4e052a82dd2ea85194b65ff9a3ca5210cc1e5
usarana <43117189+usarana@users.noreply.github.com>
2020-10-28 03:04:13 -0700

    Update e.bat

--- e.bat
+++ e.bat
@@ -1,29 +1,38 @@
 @echo off
-title cmdOS 4
+title cmdOS %1
 cd .acc
 if exist temp.txt (del temp.txt && goto l) else (goto l)
 :l
 color E
 cls
-echo Hello, %1!
+echo Hello, %2!
 echo.
-:qn
 cd..
+:qn
 echo Here are some programs you can use.
-cmdmenusel E870 "Timer" "Calculator" "Command Line" "Text editor"
+cmdmenusel E870 "Timer" "Calculator" "Command Line" "Text Editor" "About"
 if %errorlevel%==1 goto t
 if %errorlevel%==2 goto c
 if %errorlevel%==3 goto m
 if %errorlevel%==4 goto k
+if %errorlevel%==5 goto wqw
 :t
-start Library\timer.bat
+cd Library && timer.bat
 exit
 :c
-start Library\calculator.bat
+cd Library && calculator.bat
 exit
 :m
-start Library\command.bat %1
+cd Library && command.bat %1 %2 %3
 exit
 :k
-start Library/text.bat
+cd Library && text.bat
 exit
+:wqw
+cls
+echo Version: %1
+echo Command Line Version: %3
+echo Username: %2
+echo.
+cmdmenusel E870 "Update" "Go back"
+if %errorlevel% == 1 (start update.bat %1 && exit) else (cls && echo Hello, %2! && echo. && goto qn)
